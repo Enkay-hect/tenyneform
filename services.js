65a7ae555f8e018070505d6562b6be458da98408
Enkay-hect <fridaynkeneabasi@gmail.com>
2023-11-29 13:49:34 +0100

    program categeory get request

--- services.js
+++ services.js
@@ -111,3 +111,48 @@ const JOB_CATEGORY = {
     
     
 }
+
+const CREATE_PROGRAM = {
+
+    createprogram : function(title, programTitle, reviews, subtitle, description,
+        features, start_date, end_date, price,learning_scheme,why,prerequisite,best_fit,
+        program_flow, rating,instructor_name, instructor_details){
+        const FormD = new FormData();
+
+        FormD.append("description", description);
+        FormD.append("title", title);
+        FormD.append("programTitle", programTitle);
+        FormD.append("reviews", reviews);
+        FormD.append("subtitle", subtitle);
+        FormD.append("features", features);
+        FormD.append("start_date", start_date);
+        FormD.append("end_date", end_date);
+        FormD.append("price", price);
+        FormD.append("learning_scheme", learning_scheme);
+        FormD.append("why", why);
+        FormD.append("prerequisite", prerequisite);
+        FormD.append("best_fit", best_fit);
+        FormD.append("program_flow", program_flow);
+        FormD.append("rating", rating);
+        FormD.append("instructor_name", instructor_name);
+        FormD.append("instructor_details", instructor_details);
+        FormD.append("image", $("#image").prop("files")[0]);
+
+        $.ajax({
+            type: 'POST',
+            url: 'http://localhost:8000/api/createprogramcategory',
+            cache: false,
+            processData: false, 
+            data: FormD,
+                            
+            contentType: false,
+
+                error: function (request, status, error) {
+                    console.error(request.responseText);
+                }
+        });
+
+
+    }
+
+}
